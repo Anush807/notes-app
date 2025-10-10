19911f471d068b39b7fb52b2881e931ef55ff5a9
Anush807 <anushpoojary0101@gmail.com>
2025-10-10 23:50:26 +0530

    fe

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
 import { authRoute } from './routes/authRoute';
 import { notesRoute } from './routes/notesRoute';
+import { meRoute } from "./routes/meRoute"
 import { cors } from 'hono/cors';
 
 const app = new Hono();
 app.use('/api/*', cors())
+// app.get("/api/v1/me", meRoute)
 app.route("/api/v1/auth", authRoute);
 app.route("/api/v1/notes",notesRoute);
 
